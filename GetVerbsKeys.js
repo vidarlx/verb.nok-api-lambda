199850d4148cd789d4bb38808f45959aa24b9140
vidarlx <lax@fea.st>
2018-07-08 13:41:06 +0200

    Return norsk/polsk verbs list

--- GetVerbsKeys.js
+++ GetVerbsKeys.js
@@ -1,3 +1,26 @@
-exports.handler = function(event, context, callback) { 
-  callback(null, "200!");
+const AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+
+AWS.config.update({
+  region: "eu-west-2"
+});
+
+exports.handler = function(event, context, callback) {
+
+    var params = {
+        TableName: "NorskVerb",
+        ProjectionExpression: "norsk_verb, polsk_verb"
+    };
+    
+    ddb.scan(params, onVerbsFound);
+    
+    callback(null, "200!");
+}
+
+function onVerbsFound(err, data) {
+    if (err) {
+       return callback(err);
+    }
+    
+    callback(null, data);
 }
