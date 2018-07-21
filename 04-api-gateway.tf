f1d23540f0ef992c5d202e6ab6e3bcac4bd91cd2
vidarlx <lax@fea.st>
2018-07-22 00:15:50 +0200

    API Gateway: GetVerbs endpoint

--- 04-api-gateway.tf
+++ 04-api-gateway.tf
@@ -6,15 +6,24 @@ resource "aws_api_gateway_rest_api" "verbs_nok_api" {
   }
 }
 
-resource "aws_api_gateway_resource" "VerbsNokAPIGetMethod" {
+resource "aws_api_gateway_resource" "verbs_nok_api_verbs_resource" {
   rest_api_id = "${aws_api_gateway_rest_api.verbs_nok_api.id}"
   parent_id   = "${aws_api_gateway_rest_api.verbs_nok_api.root_resource_id}"
   path_part   = "get-verbs"
 }
 
-resource "aws_api_gateway_method" "VerbsNokAPIGetVerbsEndpoint" {
+resource "aws_api_gateway_method" "verbs_nok_api_get_verbs_method" {
   rest_api_id   = "${aws_api_gateway_rest_api.verbs_nok_api.id}"
-  resource_id   = "${aws_api_gateway_resource.verbs_nok_api.id}"
+  resource_id   = "${aws_api_gateway_resource.verbs_nok_api_verbs_resource.id}"
   http_method   = "GET"
   authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "verbs_nok_api_get_verbs_integration" {
+  rest_api_id             = "${aws_api_gateway_rest_api.verbs_nok_api.id}"
+  resource_id             = "${aws_api_gateway_resource.verbs_nok_api_verbs_resource.id}"
+  http_method             = "${aws_api_gateway_method.verbs_nok_api_get_verbs_method.http_method}"
+  integration_http_method = "GET"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:eu-west-2:lambda:path/2015-03-31/functions/${aws_lambda_function.GetVerb.arn}/invocations"
 }
