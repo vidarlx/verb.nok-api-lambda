3abf8f7428a1d5763085fb69a633c5d47ce010c5
vidarlx <lax@fea.st>
2018-07-22 11:30:08 +0200

    All lambdas in one bundle, automated

--- 03-lambda.tf
+++ 03-lambda.tf
@@ -1,9 +1,15 @@
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "lambda"
+  output_path = "lambda.zip"
+}
+
 resource "aws_lambda_function" "GetVerbsKeys" {
-  filename         = "GetVerbsKeys.zip"
+  filename         = "lambda.zip"
   function_name    = "GetVerbsKeys"
   role             = "${aws_iam_role.iam_for_lambda.arn}"
   handler          = "GetVerbsKeys.handler"
-  source_code_hash = "${base64sha256(file("GetVerbsKeys.zip"))}"
+  source_code_hash = "${data.archive_file.lambda_zip.output_base64sha256}"
   runtime          = "nodejs6.10"
 
   environment {
@@ -14,10 +20,10 @@ resource "aws_lambda_function" "GetVerbsKeys" {
 }
 
 resource "aws_lambda_function" "GetVerb" {
-  filename         = "GetVerb.zip"
+  filename         = "lambda.zip"
   function_name    = "GetVerb"
   role             = "${aws_iam_role.iam_for_lambda.arn}"
   handler          = "GetVerb.handler"
-  source_code_hash = "${base64sha256(file("GetVerb.zip"))}"
+  source_code_hash = "${data.archive_file.lambda_zip.output_base64sha256}"
   runtime          = "nodejs6.10"
 }
