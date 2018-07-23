bee74b56829b4b54f287d98d4976e61a1ad3b72f
vidarlx <lax@fea.st>
2018-07-23 22:34:00 +0200

    API Gateway stages (test, prod)

--- 01-main.tf
+++ 01-main.tf
@@ -1,3 +1,7 @@
 provider "aws" {
   region = "eu-west-2"
 }
+
+variable "region" {
+  default = "eu-west-2"
+}
---
+++ 04-2-api-gateway-deployment.tf
@@ -0,0 +1,16 @@
+resource "aws_api_gateway_stage" "test" {
+  stage_name    = "prod"
+  rest_api_id   = "${aws_api_gateway_rest_api.verbs_nok_api.id}"
+  deployment_id = "${aws_api_gateway_deployment.verbs_nok_api_test_deployment.id}"
+}
+
+resource "aws_api_gateway_deployment" "verbs_nok_api_test_deployment" {
+  depends_on = ["aws_api_gateway_integration.verbs_nok_api_get_verbs_integration"]
+
+  rest_api_id = "${aws_api_gateway_rest_api.verbs_nok_api.id}"
+  stage_name  = "test"
+
+  variables = {
+    "env" = "test"
+  }
+}
